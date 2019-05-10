257de7cfacd697835bb9141a7b9fe3b6ef4ec4c4
Lucas Fontes <lucasfcartaxo@gmail.com>
2019-05-10 17:51:27 -0300

    push frontend project readable for nanodegree react

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+        <div className="alert alert-primary" role="alert">
+          Bootstrap Started!
+        </div>
+    </div>
+  );
+}
+
+export default App;
